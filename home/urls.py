ee193121f6a2180f432f1e0f8302d68a6e0b92ef
Zeeshan17CO50 <mohammedsiddiquezee@gmail.com>
2020-03-12 23:54:40 +0530

    linking of all pages

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home/', views.index, name='index')
+    path('home/', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('blog/', views.blog, name='blog'),
+    path('single_blog/', views.single_blog, name='single_blog'),
+    path('contact/', views.contact, name='contact'),
+    path('profile/', views.profile, name='profile'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def blog(request):
+    return render(request, 'blog.html')
+
+
+def single_blog(request):
+    return render(request, 'single_blog.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def profile(request):
+    return render(request, 'profile.html')
